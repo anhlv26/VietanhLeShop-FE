9bf2462ca8db556b7896071f29586934ec36ea8b
vietanh <vietanhle2662001@gmail.com>
2024-06-29 16:24:44 +0700

    install redux-reduxtoolkit and config structure project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-// import { useState } from "react";
-
 import { Box } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/slice/counterSlice";
+import { RootState } from "./redux/store";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <Box>
-      Lo lo lo
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
     </Box>
   );
 }
