8aebc97745849adef73ab7d85f49058bc81f0eaa
vietanh <vietanhle2662001@gmail.com>
2024-06-30 23:33:47 +0700

    build ui header

--- src/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/HeaderComponent/HeaderComponent.tsx
@@ -1,7 +1,127 @@
-import React from "react";
+import { Badge, Box, Button, Grid } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import InputBase from "@mui/material/InputBase";
+import Divider from "@mui/material/Divider";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const HeaderComponent = () => {
-  return <div>HeaderComponent</div>;
+  const headerHeight = "80px";
+  return (
+    <Box sx={{ width: "100%", padding: "0 72px" }}>
+      <Grid
+        container
+        sx={{
+          height: headerHeight,
+          width: "100%",
+        }}
+      >
+        <Grid
+          item
+          md={1}
+          sx={{
+            height: "100%",
+            lineHeight: headerHeight,
+          }}
+        >
+          VANH
+        </Grid>
+        <Grid
+          item
+          md={8}
+          sx={{
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Paper
+            component="form"
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              width: "100%",
+              height: 40,
+              border: "1px solid #e5e5e5",
+              boxShadow: "none",
+              borderRadius: "6px",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                paddingLeft: "20px",
+              }}
+            >
+              <SearchIcon sx={{ fontWeight: "400", opacity: "0.5" }} />
+            </Box>
+            <InputBase
+              sx={{ ml: 1, flex: 1, fontWeight: "400" }}
+              placeholder="100% hàng tuyển chọn"
+              inputProps={{ "aria-label": "search google maps" }}
+            />
+            <Divider sx={{ height: 28, ml: 0.5 }} orientation="vertical" />
+            <Button
+              color="primary"
+              sx={{
+                fontSize: "14px",
+                height: "100%",
+              }}
+            >
+              Tìm kiếm
+            </Button>
+          </Paper>
+        </Grid>
+        <Grid
+          item
+          md={3}
+          sx={{
+            height: "100%",
+            lineHeight: headerHeight,
+            paddingLeft: "60px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+          }}
+        >
+          <Button
+            sx={{
+              borderRadius: "6px",
+            }}
+            startIcon={<HomeIcon sx={{ fontSize: "30px !important" }} />}
+          >
+            Trang chủ
+          </Button>
+          <Button
+            sx={{ borderRadius: "6px" }}
+            startIcon={<EmojiEmotionsIcon />}
+          >
+            Tài khoản
+          </Button>
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+          <Button sx={{ borderRadius: "6px" }}>
+            <Badge
+              badgeContent={2}
+              color="primary"
+              sx={{
+                "& .MuiBadge-badge": {
+                  fontSize: "10px",
+                  maxWidth: 12,
+                  height: "16px",
+                  borderRadius: "16px",
+                },
+              }}
+            >
+              <ShoppingCartIcon />
+            </Badge>
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default HeaderComponent;
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,18 +1,18 @@
 import { createTheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
 
-declare module '@mui/material/styles' {
-    interface Theme {
-      status: {
-        danger: string;
-      },
-    }
-    interface ThemeOptions {
-      status?: {
-        danger?: string;
-      },
-    }
+declare module "@mui/material/styles" {
+  interface Theme {
+    status: {
+      danger: string;
+    };
   }
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
 
 // A custom theme for this app
 const theme = createTheme({
@@ -25,6 +25,18 @@ const theme = createTheme({
     },
     error: {
       main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+      },
     },
   },
 });
